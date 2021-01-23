427d99ad0b469c0be7f28f61b4b2e63787868ff3
Naim <gmnaim3336@gmail.com>
2021-01-23 13:03:37 +0600

    Some basic functions added.

--- apps/core/base/utils/basics.py
+++ apps/core/base/utils/basics.py
@@ -13,3 +13,48 @@ def random_hex_code(length: int = 8) -> str:
     """
     return uuid.uuid4().hex[:length]
 
+
+def json_parameter_validation(json_data, required_parameters):
+    """ Check parameter is available in json or not
+    :parameter:
+        json_data: dict, required
+            A dictionary that should be validate by parameter are available or not
+        required_params: list, required
+            Those list of params that must be available on
+    """
+    for param in required_parameters:
+        if json_data.get(param) is None:
+            return param
+
+
+def get_user_ip_address(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+def get_user_browser_details(request):
+    return request.headers.get('User-Agent')
+
+
+def store_user_activity(request, store_json='', description='') -> None:
+    """
+       Store user activity into ActivityLog table for future reference
+
+       :parameter
+           request (obj): django request object
+           store_json (str): dumps json for request/response data
+           description (str): basic description to represent userside view
+   :return:
+   """
+
+    # from apps.core.account.models import ActivityLog
+    # ActivityLog.objects.create(store_json=store_json,
+    #                            description=description,
+    #                            ip_address=get_user_ip_address(request),
+    #                            browser_details=get_user_browser_details(request))
+
+
